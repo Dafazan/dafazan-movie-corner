803e88b83809d1fd46f0bbea3d36885ba714107d
dafazanlbs <dafazanlbs@gmail.com>
2024-04-11 08:59:07 +0000

    tes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 
 import Search from '@/app/components/Pages/Search'
 import Popular from '@/app/components/Pages/Popular'
+import Popular from '@/app/components/Navigation/TopNav'
 
 
 export default function Home() {
   return (
     <div className="W-full h-full">
+      <TopNav />
       <Search />
       <Popular />
     </div>
